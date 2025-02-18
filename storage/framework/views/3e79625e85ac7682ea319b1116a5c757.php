71b810b9f0341a6659aeeccb5bb3ed4c2bffed66
Ruhollah Erfan Ribas Malekzadeh <rribasm2324@politecnics.barcelona>
2025-02-18 19:01:41 +0100

    pruebas
    porbadno con pablo

---
+++ storage/framework/views/3e79625e85ac7682ea319b1116a5c757.php
@@ -0,0 +1,8 @@
+<?php $__env->startSection('PantallaDeInicio'); ?>
+<div class="full-index-1">
+</div>
+<?php $__env->stopSection(); ?>
+
+
+
+<?php echo $__env->make('layouts', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\sing_drink\resources\views/proves/index.blade.php ENDPATH**/ ?>
