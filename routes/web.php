c0ee9968f0bcda4ceee19d4b3033f87de7d05716
RogerOrti <rortim2223@politecnics.barcelona>
2025-02-21 19:39:12 +0100

    Add login register
    Pending to test and review with another routes

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,17 @@ Route::get('/', function () {
 });
 
 Route::get('/register', [UsuariController::class,'showRegister'])->name('register');
-Route::post('/register', [UsuariController::class, 'register']);
+Route::post('/register', [UsuariController::class, 'register'])->name('usuari.registre');
 
 Route::get('/login', [UsuariController::class,'showLogin'])->name('login');
 Route::post('/login', [UsuariController::class, 'login']);
+Route::post('', [UsuariController::class,''])->name('logout');
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function ($id) {
+        $user = Auth::user();
+
+        return view('', compact('user'));
+    });
+ });
