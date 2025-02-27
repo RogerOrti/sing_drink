24b7b942fc7287f46a9527a3bbfc6742c3e373ec
Ruhollah Erfan Ribas Malekzadeh <rribasm2324@politecnics.barcelona>
2025-02-27 18:58:23 +0100

    Create musics.blade.php

---
+++ resources/views/musics/musics.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts')
+
+@section('fonsPantalla')
+<img src="{{ asset('image/erfan/indexMusics.mp4') }}" width="100%" height="100%" alt="GIF de imagen" style="position:absolute">
+@endsection
+
+@section('PantallaDeInicio')
+<div>
+    <h1>
+        MÚSIC
+    </h1>
+</div>
+
+@endsection
+
+@section('Contenido_Adicional')
+
+@endsection
+
+
