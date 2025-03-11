51669fa893c934ccbcd31dd8d1a705fb9bbfc441
Erfan Ribas <rribasm2324@politecnics.barcelona>
2025-03-11 20:15:43 +0100

    carrusel incorporado
    jordi solving tomorrow

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sticky Navbar with Full Screen Div</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
-    @vite(['resources/css/app.css', 'resources/css/app.scss', 'resources/js/app.js', 'resources/css/erfan.css', 'resources/css/pablo.css' ])
+    @vite(['resources/css/app.css', 'resources/css/app.scss', 'resources/js/app.js', 'resources/css/erfan.css', 'resources/css/pablo.css', 'resources/css/style.css', 'resources/js/main.js' ])
 </head>
 
 <body>
--- storage/framework/views/3e79625e85ac7682ea319b1116a5c757.php
+++ storage/framework/views/3e79625e85ac7682ea319b1116a5c757.php
@@ -1,22 +1,60 @@
-<?php $__env->startSection('fonsPantalla'); ?>
-    <img src="<?php echo e(asset('image/200.gif')); ?>" width="100%" height="100%" alt="GIF de imagen" style="position:absolute">
-<?php $__env->stopSection(); ?>
-
-
 <?php $__env->startSection('PantallaDeInicio'); ?>
-    <div class="full-index-1">
-        <div class="container">
-            <h2>SING&DRINK</h2>
-            <div class="InicioDescripcion">
-                <p>"Sing and Drink es un proyecto que ofrece una experiencia única en bares, donde músicos en vivo crean un ambiente animado mientras los asistentes disfrutan de una variedad de bebidas. Con una selección musical ecléctica, el evento promueve la interacción social y el disfrute de la música en un entorno relajado."</p>
-            </div>
-            <div class="btn btn-secondary me-2 IndexBoton">
-                <p>Nuestros Eventos!!!</p>
-            </div>
-        </div>
-    </div>
+<div id="quisom" style="background-image: url('<?php echo e(url('image/200.gif')); ?>');">
+    <h1 class="display-1 fw-bold text-center larger-title">QUI SOM</h1>
+    <p class="display-5  text-center ">-</p>
+    <p class="display-6 text-center"><strong>Sing&Drink</strong></p>
+</div>
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('Contenido_Adicional'); ?>
+<div class="carouselIndex">
+    <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="<?php echo e(asset('img/fondo.jpg')); ?>" alt="Imagen 1">
+            </div>
+            <div class="swiper-slide">
+                <img src="<?php echo e(asset('img/fondo.jpg')); ?>" alt="Imagen 2">
+            </div>
+            <div class="swiper-slide">
+                <img src="<?php echo e(asset('img/fondo.jpg')); ?>" alt="Imagen 3">
+            </div>
+            <div class="swiper-slide">
+                <img src="<?php echo e(asset('img/fondo.jpg')); ?>" alt="Imagen 4">
+            </div>
+
+        </div>
+        <div class="swiper-pagination"></div>
+    </div>
+</div>
 <?php $__env->stopSection(); ?>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        var swiper = new Swiper(".mySwiper", {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            spaceBetween: 50,
+            initialSlide: 2,
+            coverflowEffect: {
+                rotate: 10,
+                stretch: 0,
+                depth: 500,
+                modifier: 1,
+                slideShadows: true,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+            },
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+        });
+    });
+</script>
+
+
 
 <?php echo $__env->make('layouts', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\sing_drink\resources\views/proves/index.blade.php ENDPATH**/ ?>
