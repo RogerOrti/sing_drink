23203b3bc0951b69c566d839ebc4915ca40538ae
Erfan Ribas <rribasm2324@politecnics.barcelona>
2025-03-21 20:15:42 +0100

    component carta locals

---
+++ resources/js/components/locals/cartaLocal.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div class="cardsLocals" v-for="cartaLocal in cartasLocals" :key="cartaLocal.id">
+     <div class="card" style="width: 18rem;">
+        <img src="image" class="card-img-top" alt="imatge restuarant">
+         <div class="card-body">
+            <h5 class="card-title"> {{ cartaLocal.nom_local }}</h5>
+            <h5 class="card-title"> {{ cartaLocal.direccio }}</h5>
+            <a href="#" class="btn btn-primary">Saber més</a>
+        </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+    data() {
+        return {
+            cartasLocals: []
+        }
+    },
+
+    mounted() {
+        // Realizamos la solicitud HTTP con axios
+        axios.get("http://localhost/sing_drink/public/locals")
+            .then(response => {
+                // Asignamos los datos de la respuesta a cartasLocals
+                this.cartasLocals = response.data;
+            })
+            .catch(error => {
+                console.error("Error al cargar los datos:", error);
+            });
+
+}
+}
+</script>
+<style>
+
+</style>
