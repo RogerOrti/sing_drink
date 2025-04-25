0655888d005776e756e9963761f78edfc315cc0e
Pablo Lin <162104715+LOSTYRL6@users.noreply.github.com>
2025-04-25 17:29:09 +0200

    ko

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -133,5 +133,27 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const navbar = document.querySelector('.custom-navbar');
+
+        function updateNavbarBackground() {
+            const scrollY = window.scrollY;
+            const isSmallScreen = window.innerWidth < 992;
+
+            if (scrollY > 100 || isSmallScreen) {
+                navbar.classList.add('scrolled');
+            } else {
+                navbar.classList.remove('scrolled');
+            }
+        }
+
+        // Ejecutar al cargar y cuando cambia el tamaño o se hace scroll
+        updateNavbarBackground();
+        window.addEventListener('scroll', updateNavbarBackground);
+        window.addEventListener('resize', updateNavbarBackground);
+    });
+</script>
+
 
 </html>
