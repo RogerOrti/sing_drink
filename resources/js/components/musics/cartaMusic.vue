2ffcbdef08dcbb9c977924155e343cc531c33ddd
Pablo Lin <162104715+LOSTYRL6@users.noreply.github.com>
2025-05-21 16:13:24 +0200

    asa

--- resources/js/components/musics/cartaMusic.vue
+++ resources/js/components/musics/cartaMusic.vue
@@ -1,38 +1,59 @@
 <template>
-        <div class="coleccioMusics">
-            <div class="cardsLocals" v-for="music in musics" :key="music.id_user">
-                <div class="cardMusics">
-                    <img :src="music.multimedia_music[0].ruta" class="cardMusics" alt="">3ex
-                    <!-- <img src="{{ asset('image/erfan/Anthony_Kiedis.png') }}" class="cardMusics" alt=""> -->
-                    <!-- <img class="cardMusics" :src="music.multimedia.ruta"/> -->
-                    <div>
-                        <!-- <a href="music" class=" btn btnMusics btn-primary">Saber más</a> -->
-                        <a :href="'music/' + music.id_user " class="btn btnMusics btn-primary">Saber més</a>
-                        <img class="play card-img-top" :src="('image/erfan/logoPlay2.webp')" alt="">
-                    </div>
+    <div class="coleccioMusics">
+        <div class="cardsLocals" v-for="music in musics" :key="music.id_user">
+            <div class="cardMusics">
+                <img
+                    :src="music.multimedia_music[0].ruta"
+                    class="cardMusics"
+                    alt=""
+                />
+                <!-- <img src="{{ asset('image/erfan/Anthony_Kiedis.png') }}" class="cardMusics" alt=""> -->
+                <!-- <img class="cardMusics" :src="music.multimedia.ruta"/> -->
+                <div>
+                    <!-- <a href="music" class=" btn btnMusics btn-primary">Saber más</a> -->
+                    <a
+                        :href="'music/' + music.id_user"
+                        class="btn btnMusics btn-primary"
+                        >Saber més</a
+                    >
+                    <img
+                        class="play card-img-top"
+                        :src="'image/erfan/logoPlay2.webp'"
+                        alt=""
+                    />
                 </div>
             </div>
         </div>
-        <div class="pagination d-flex justify-content-center align-items-center my-4">
-        <button class="btn btn-secondary mx-2" :disabled="currentPage === 1" @click="cambiarPagina(currentPage - 1)">
-          Anterior
+    </div>
+    <div
+        class="pagination d-flex justify-content-center align-items-center my-4"
+    >
+        <button
+            class="btn btn-secondary mx-2"
+            :disabled="currentPage === 1"
+            @click="cambiarPagina(currentPage - 1)"
+        >
+            Anterior
         </button>
         <span>Página {{ currentPage }} de {{ lastPage }}</span>
-        <button class="btn btn-secondary mx-2" :disabled="currentPage === lastPage" @click="cambiarPagina(currentPage + 1)">
-          Siguiente
+        <button
+            class="btn btn-secondary mx-2"
+            :disabled="currentPage === lastPage"
+            @click="cambiarPagina(currentPage + 1)"
+        >
+            Siguiente
         </button>
-      </div>
-  </template>
+    </div>
+</template>
 
-  <script>
-  export default {
-
+<script>
+export default {
     data() {
-      return {
-        musics: [],
-        currentPage: 1,
-        lastPage: 1
-      }
+        return {
+            musics: [],
+            currentPage: 1,
+            lastPage: 1,
+        };
     },
 
     mounted() {
@@ -40,26 +61,27 @@
     },
 
     methods: {
-      GetMusic(page = 1) {
-        axios.get("musics/paginado", { params: { page } })
-          .then((response) => {
-            this.musics = response.data.data;
-            this.currentPage = response.data.meta.current_page;
-            this.lastPage = response.data.meta.last_page;
-          })
-          .catch((error) => {
-            console.error("Error al cargar los datos:", error);
-          });
-      },
-      cambiarPagina(nuevaPagina) {
-        if (nuevaPagina >= 1 && nuevaPagina <= this.lastPage) {
-          this.GetMusic(nuevaPagina);
-        }
-      }
-    }
-  }
-  </script>
+        GetMusic(page = 1) {
+            axios
+                .get("musics/paginado", { params: { page } })
+                .then((response) => {
+                    this.musics = response.data.data;
+                    this.currentPage = response.data.meta.current_page;
+                    this.lastPage = response.data.meta.last_page;
+                })
+                .catch((error) => {
+                    console.error("Error al cargar los datos:", error);
+                });
+        },
+        cambiarPagina(nuevaPagina) {
+            if (nuevaPagina >= 1 && nuevaPagina <= this.lastPage) {
+                this.GetMusic(nuevaPagina);
+            }
+        },
+    },
+};
+</script>
 
-  <style scoped>
-  /* Aquí va el estilo personalizado */
-  </style>
+<style scoped>
+/* Aquí va el estilo personalizado */
+</style>
--- resources/views/local/local.blade.php
+++ resources/views/local/local.blade.php
@@ -1,47 +1,43 @@
 @extends('layouts')
 
 @section('fonsPantalla')
-{{-- <img src="{{ asset('image/erfan/fotoLocal.jpg') }}" width="100%" class="fosc" height="100%" alt="GIF de imagen" style="position:absolute"> --}}
-<img src="{{ asset($local->multimedia_local->ruta) }}" class="fotoPortadaCantant" alt="">
+    {{-- <img src="{{ asset('image/erfan/fotoLocal.jpg') }}" width="100%" class="fosc" height="100%" alt="GIF de imagen" style="position:absolute"> --}}
+    <img src="{{ asset($local->multimedia_local->ruta) }}" class="fotoPortadaCantant" alt="">
 @endsection
 
 @section('PantallaDeInicio')
-<div id="app" data-id="{{ $id }}">
-    <pagina-local></pagina-local>
-    {{-- <div id="quisom">
+    <div id="app" data-id="{{ $id }}">
+        <pagina-local></pagina-local>
+        {{-- <div id="quisom">
         <h1 class="display-1 fw-bold text-center larger-title">BAR PEPE</h1>
     </div> --}}
-</div>
-
+    </div>
 @endsection
 
 @section('Contenido_Adicional')
-
-<div id="app5" data-id="{{ $id }}">
-    <mapalocal></mapalocal>
-</div>
-<div class="fons d-flex justify-content-center ">
-    <div class="m-5">
-        <h1 class="titolHoraris">OPINIONS</h1>
-        <p class="review"> Joselito - Muy buen ambiente</p>
-        <p class="review"> Joselito - Me gusta su fruta</p>
-        <p class="review"> Joselito - Vaya guapada de sitio</p>
-
+    <div id="app5" data-id="{{ $id }}">
+        <mapalocal></mapalocal>
     </div>
-    <hr class="hrLocal">
-    <div class="m-5">
-        <h1 class="titolHoraris">HORARIS</h1>
-        <ul class="horaris">
-            <li>Dilluns: 10:00 - 23:00</li>
-            <li>Dimarts: 10:00 - 23:00</li>
-            <li>Dimecres: 10:00 - 23:00</li>
-            <li>Dijous: 10:00 - 23:00</li>
-            <li>Divendres: 10:00 - 23:00</li>
-            <li>Dissabte: 10:00 - 23:00</li>
-            <li>Tancat</li>
-        </ul>
-    </div>
-</div>
+    <div class="fons d-flex justify-content-center ">
+        <div class="m-5">
+            <h1 class="titolHoraris">OPINIONS</h1>
+            <p class="review"> Joselito - Muy buen ambiente</p>
+            <p class="review"> Joselito - Me gusta su fruta</p>
+            <p class="review"> Joselito - Vaya guapada de sitio</p>
 
+        </div>
+        <hr class="hrLocal">
+        <div class="m-5">
+            <h1 class="titolHoraris">HORARIS</h1>
+            <ul class="horaris">
+                <li>Dilluns: 10:00 - 23:00</li>
+                <li>Dimarts: 10:00 - 23:00</li>
+                <li>Dimecres: 10:00 - 23:00</li>
+                <li>Dijous: 10:00 - 23:00</li>
+                <li>Divendres: 10:00 - 23:00</li>
+                <li>Dissabte: 10:00 - 23:00</li>
+                <li>Tancat</li>
+            </ul>
+        </div>
+    </div>
 @endsection
-
